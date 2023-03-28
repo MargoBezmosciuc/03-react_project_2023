0764902c2f36adb54481135714f53965b8063039
margo.bezmosciuc <margo.bezmosciuc@gmail.com>
2023-03-28 10:53:34 +0200

    Project Completed

--- 03_react_project/src/App.js
+++ 03_react_project/src/App.js
@@ -1,11 +1,24 @@
-import logo from "./logo.svg";
-//import './App.css';
-import AddUsers from "./components/Users/AddUsers";
+import React, { useState } from 'react';
+
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
   return (
     <div>
-      <AddUsers />
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
